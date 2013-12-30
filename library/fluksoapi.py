0b43c224df101e26a376cd1d475e5bcb2b79cd27
Roel De Coninck <roel.deconinck@3e.eu>
2013-12-30 04:53:21 +0100

    Splitted the fluksoapi in get and save method, script for pulling all data and saving csv files

--- library/fluksoapi.py
+++ library/fluksoapi.py
@@ -8,29 +8,25 @@ from pandas import TimeSeries
 import requests
 import os
 from time import mktime,strftime
+import pdb
 
-def fluksoPower(start = dt(2013,1,21,0), end = dt.now(),
-                sensor = '78022059b66cec92ebf8ea6ca98f979b',
-                token = '8a11d349ab2b4ce43eea525d0ea26186',
-                resolution = 'minute',
-                unit = 'watt',
-                csvpath = r'\\fs3E01\\3EProjectsBelgium\\iLAB_InnovationLaboratory\\CommonData\\Flukso\\Flukso1',
-                fileNamePrefix = "3E_FLUKSO_1"):
-    """
-    Function for downloading data from flukso meters
+def pull_api(sensor, token, unit, 
+             start = dt(2013,1,1,0), end = dt.now(), resolution = 'minute'):
+   
+    """     
+   
+    Function for downloading data from fluksometers
     
     Parameters
     ----------
-    - start : start time (default = installation of first flukso)
+    - start : start time (default = first of january 2013)
     - end : end time (default = now)
-    - sensor :  sensor name (from the flukso.net user tab)
-    - token :  sensor token (from the flukso.net user tab)
+    - sensor :  sensor name (from the flukso.net sensor tab)
+    - token :  sensor token (from the flukso.net sensor tab)
     - resolution: time resolution (e.g. minute, 15min, hour, day, week, month, 
       year, decade, night)
-    - unit: unit of measurements (e.g. watt, kwhperyear)
-    - csvpath : path for storing output file
-    - fileNamePrefix: flukso name for the output file
-    
+    - unit: unit of measurements (e.g. watt, kwhperyear, lperday)
+
     Note
     ----
     The Flukso Server will automatically restrict the data to what's available
@@ -38,7 +34,7 @@ def fluksoPower(start = dt(2013,1,21,0), end = dt.now(),
     
     Returns
     -------
-    CSV file with the requested timeseries, stored at the csvpath
+    Array with the raw data.  Use the save2csv function to parse and save.
     
     """
     start_unixtime = str(int(mktime(start.timetuple())))
@@ -59,25 +55,37 @@ def fluksoPower(start = dt(2013,1,21,0), end = dt.now(),
     try:    
         s = requests.Session()
         r = s.get(url, params = payload, headers = headers, verify=False)
-        array = r.json
     except:
         print "-------> Problem with HTTP request to Flukso <-------"
+    
+    return r
+
+
+def save2csv(r, csvpath=None, fileNamePrefix=''):
+    """
+    Parse and save to csv
+    """
+    
     
     # Create Dataframe   
     try:
         d = {}
-        for tup in array:
+        for tup in r.json():
             d[dt.fromtimestamp(tup[0])] = tup[1]
+        #pdb.set_trace()
         Ts = TimeSeries(data=d)
-        Ts = Ts[Ts != 'nan']
-        s = strftime("%Y-%m-%d_%H-%M-%S",Ts.index[0].timetuple())    
-        e = strftime("%Y-%m-%d_%H-%M-%S",Ts.index[-1].timetuple())
-        Ts.to_csv(os.path.join(csvpath, fileNamePrefix + '_FROM_' + s + 
-                                        '_TO_' + e + '.csv'))    
+        # this line gives an error.  Should be checked, but for now I keep the nan's        
+        # Ts = Ts[Ts != 'nan']
+        
     except:
         print "-------> Problem with Flukso data parsing <-------"
+        raise
 
-
-if __name__=='__main__':
-    # Get data of 3E's First Flukso Meter (Default Settings)
-    fluksoPower()    
+    # save to file
+    if csvpath is None:
+        csvpath = os.getcwd()
+    s = strftime("%Y-%m-%d_%H-%M-%S",Ts.index[0].timetuple())    
+    e = strftime("%Y-%m-%d_%H-%M-%S",Ts.index[-1].timetuple())
+    Ts.to_csv(os.path.join(csvpath, fileNamePrefix + '_FROM_' + s + 
+                                    '_TO_' + e + '.csv'))    
+   
---
+++ scripts/extract_flukso_api.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Script to extract all minute sensor data from the flukso server through the 
+flukso api.
+
+
+Created on Mon Dec 30 04:24:28 2013 by Roel De Coninck
+"""
+
+import os, sys
+import inspect
+
+script_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+# add the path to opengrid to sys.path
+sys.path.append(os.path.join(script_dir, os.pardir, os.pardir))
+from opengrid.library.houseprint import Houseprint
+from opengrid.library import fluksoapi
+
+# script settings ############################################################
+extract_all = True
+save_all = True
+
+##############################################################################
+
+hp = Houseprint()
+all_sensordata = hp.get_all_fluksosensors()
+
+if extract_all:
+    for flukso_id in all_sensordata.keys():
+        for sensor_id, s in all_sensordata[flukso_id].items():
+            # sensor_id is 1-6, s is {}
+            if s is not None:
+                # determine the type of the measurement to set the unit                
+                t = s['Type'].lower()
+                if t.startswith('ele'):
+                    unit = 'watt'
+                else:
+                    unit = 'lperday'
+                # pull the data from the flukso server
+                r = fluksoapi.pull_api(sensor=s['Sensor'], token=s['Token'],
+                                       unit=unit)
+
+                if save_all:
+                    fluksoapi.save2csv(r, csvpath=None, fileNamePrefix=s['Sensor'])
+                
+            
