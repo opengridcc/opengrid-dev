b382ba0a397a2da923bb01d3f59ebdd1a676fe49
Karel De Brabandere <Karel.DeBrabandere@3E.eu>
2014-11-26 22:10:15 +0100

    Documenting and plotting at minute instead of hour level

--- library/plotting.py
+++ library/plotting.py
@@ -11,31 +11,39 @@ import matplotlib.cm as cm
 from matplotlib.dates import DayLocator, AutoDateFormatter, date2num
 from matplotlib.colors import LogNorm
 
-def carpet(timeseries, vmax=None, vmin=None, zlabel=None, title=None):
+def carpet(timeseries, vmin=None, vmax=None, zlabel=None, title=None):
     """
     Draw a carpet plot of a pandas timeseries.
 
     Parameters
     ----------
     timeseries : pandas.Series
-    scale: sequence of numbers, e.g. np.linspace(0, 1000)
+    vmin, vmax : If not None, either or both of these values determine the range
+    of the z axis. If None, the range is given by the minimum and/or maximum
+    of the (resampled) timeseries.
+    zlabel, title : If not None, these determine the labels of z axis and/or
+    title. If None, the name of the timeseries is used if defined.
     """
     fig, ax = plt.subplots()
-    ts = timeseries.resample('H', how='mean', label='left',closed='left')
+    
+    #prepare data
+    ts = timeseries.resample('min', how='mean', label='left',closed='left')
+    ts.index = pd.MultiIndex.from_arrays([ts.index.hour + ts.index.minute/60., date2num(ts.index.date)])
+    df = ts.unstack()
+    mcolumns, mrows = np.meshgrid(df.columns, df.index)
+
+    #scale z axis
     if vmin is None:
         vmin = ts.min()
     if vmax is None:
         vmax = ts.max()
     scale = np.linspace(vmin, vmax)
 
-    ts.index = pd.MultiIndex.from_arrays([ts.index.hour, date2num(ts.index.date)])
-    df = ts.unstack()
-    mcolumns, mrows = np.meshgrid(df.columns, df.index)
+    #plot data
     plt.contourf(mrows, mcolumns, df, scale, cmap=cm.coolwarm, norm=LogNorm())
+    
+    #format y axis
     ax.yaxis_date()
-    plt.xlabel('Hour of the day')
-    plt.ylabel('Day of the year')
-
     ax.invert_yaxis()
     #ax.invert_yaxis() with a datetime axis obliterates the ticklocator and formatting settings
     #solution: see http://stackoverflow.com/questions/5804969/displaying-an-inverted-vertical-date-axis
@@ -44,8 +52,11 @@ def carpet(timeseries, vmax=None, vmin=None, zlabel=None, title=None):
     ax.yaxis.set_major_locator(date_locator)
     ax.yaxis.set_major_formatter(date_formatter)
 
+    #plot x, y, z labels and title
+    plt.xlabel('Hour of the day')
+    plt.ylabel('Day of the year')
     ticks = np.linspace(scale[0], scale[-1], 11, endpoint=True)
-    cb = plt.colorbar(format='%.2f',ticks=ticks)
+    cb = plt.colorbar(format='%.2f', ticks=ticks)
     if zlabel is not None:
         cb.set_label(zlabel)
     elif timeseries.name:
