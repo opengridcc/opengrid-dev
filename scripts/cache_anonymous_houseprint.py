797f3516b3cfd224b64b25e6db61fdf1cae942e3
Jan Pecinovsky <janpecinovsky@gmail.com>
2015-07-30 16:27:17 +0200

    adapted to new houseprint

--- scripts/cache_anonymous_houseprint.py
+++ scripts/cache_anonymous_houseprint.py
@@ -12,13 +12,16 @@ import inspect
 script_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 # add the path to opengrid to sys.path
 sys.path.append(os.path.join(script_dir, os.pardir, os.pardir))
-from opengrid.library.houseprint import Houseprint
+from opengrid.library import config
+from opengrid.library.houseprint import houseprint
 
 ##############################################################################
 
-hp = Houseprint()
-all_sensordata = hp.get_all_fluksosensors()
+c = config.Config()
+gjson = c.get('houseprint','json')
+
+hp = houseprint.Houseprint(gjson)
 print('Sensor data fetched')
 
-hp.save('hp_anonymous.pkl')
+hp.save('new_houseprint.pkl')
  
