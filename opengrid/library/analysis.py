74147bfc5f5acd684cfe7c2f6ec2b69e292fd331
Roel De Coninck <roeldeconinck@gmail.com>
2016-01-17 20:58:59 +0100

    Adding daily_max

--- opengrid/library/analysis.py
+++ opengrid/library/analysis.py
@@ -36,3 +36,30 @@ def daily_min(df, starttime=None, endtime=None):
         df_res.iloc[i,:] = df_day.min()
 
     return df_res
+
+
+def daily_max(df, starttime=None, endtime=None):
+    """
+
+    Parameters
+    ----------
+    df: pandas.DataFrame
+        With pandas.DatetimeIndex and one or more columns
+    starttime, endtime :datetime.time objects
+        For each day, only consider the time between starttime and endtime
+        If None, use begin of day/end of day respectively
+
+    Returns
+    -------
+    df_day : pandas.DataFrame with daily datetimeindex and maxima
+    """
+
+    df_daily_list = split_by_day(df, starttime, endtime)
+
+    # create a dataframe with correct index
+    df_res = pd.DataFrame(index=df.resample(rule='D', how='max').index, columns=df.columns)
+    # fill it up, day by day
+    for i,df_day in enumerate(df_daily_list):
+        df_res.iloc[i,:] = df_day.max()
+
+    return df_res
