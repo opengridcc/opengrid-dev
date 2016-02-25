bc70dfe60ac5c5f4b189e38b501ba719d593fd7f
Jan Pecinovsky <janpecinovsky@gmail.com>
2016-02-25 14:57:14 +0100

    rewrite split_by_day

--- opengrid/library/misc.py
+++ opengrid/library/misc.py
@@ -7,6 +7,7 @@ Created on Thu Jan  7 10:31:00 2016
 from opengrid import ureg, Q_
 import pandas as pd
 from dateutil import rrule
+import datetime as dt
 
 
 def parse_date(d):
@@ -73,31 +74,17 @@ def split_by_day(df, starttime=None, endtime=None):
     Returns
     -------
     list, one dataframe per day.
-
-    Notes
-    -----
-    The returned dataframes are simply slices of the original dataframe.
-    If both starttime and endtime are None, there is an overlap in the index
-    of subsequent dataframes whenever an index with time 00:00:00 is present.
     """
+    if df.empty:
+        return df
 
     if starttime is None:
-        timedelta_start = pd.Timedelta(seconds=0)
-    else:
-        timedelta_start = time_to_timedelta(starttime)
-
+        starttime = dt.time.min
     if endtime is None:
-        timedelta_end = pd.Timedelta(days=1)
-    else:
-        timedelta_end = time_to_timedelta(endtime)
+        endtime = dt.time.max
 
-    index_daily = df.resample(rule='D', how='max').index
-    list_df = []
-    for i in index_daily:
-        ts_start = i + timedelta_start
-        ts_end = i + timedelta_end
-        list_df.append(df.ix[ts_start:ts_end])
-
+    df = df[(df.index.time >= starttime) & (df.index.time < endtime)]  # slice between starttime and endtime
+    list_df = [group[1] for group in df.groupby(df.index.date)]  # group by date and create list
     return list_df
 
 def unit_conversion_factor(source, target):
