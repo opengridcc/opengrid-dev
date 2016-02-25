522f4d6f2624c537b975537799979978e2aa8841
Jan Pecinovsky <janpecinovsky@gmail.com>
2016-02-25 14:32:24 +0100

    made one generic daily function

--- opengrid/library/analysis.py
+++ opengrid/library/analysis.py
@@ -9,20 +9,22 @@ and return a dataframe or list of dataframes.
 import datetime as dt
 
 
-def daily_min(df, starttime=None, endtime=None):
+def daily(df, agg, starttime=None, endtime=None):
     """
 
     Parameters
     ----------
-    df: pandas.DataFrame
+    df : pandas.DataFrame
         With pandas.DatetimeIndex and one or more columns
-    starttime, endtime :datetime.time objects
+    agg : str
+        'min', 'max', or another aggregation function
+    starttime, endtime : datetime.time objects
         For each day, only consider the time between starttime and endtime
         If None, use begin of day/end of day respectively
 
     Returns
     -------
-    df_day : pandas.DataFrame with daily datetimindex and minima
+    df_day : pandas.DataFrame with daily datetimeindex and aggregated result
     """
     if df.empty:
         return df
@@ -33,33 +35,13 @@ def daily_min(df, starttime=None, endtime=None):
         endtime = dt.time.max
 
     df = df[(df.index.time >= starttime) & (df.index.time < endtime)]
-    df = df.resample('D', how='min')
+    df = df.resample('D', how=agg)
     return df
+
+
+def daily_min(df, starttime=None, endtime=None):
+    return daily(df=df, agg='min', starttime=starttime, endtime=endtime)
 
 
 def daily_max(df, starttime=None, endtime=None):
-    """
-
-    Parameters
-    ----------
-    df: pandas.DataFrame
-        With pandas.DatetimeIndex and one or more columns
-    starttime, endtime :datetime.time objects
-        For each day, only consider the time between starttime and endtime
-        If None, use begin of day/end of day respectively
-
-    Returns
-    -------
-    df_day : pandas.DataFrame with daily datetimeindex and maxima
-    """
-    if df.empty:
-        return df
-
-    if starttime is None:
-        starttime = dt.time.min
-    if endtime is None:
-        endtime = dt.time.max
-
-    df = df[(df.index.time >= starttime) & (df.index.time < endtime)]
-    df = df.resample('D', how='max')
-    return df
+    return daily(df=df, agg='max', starttime=starttime, endtime=endtime)
