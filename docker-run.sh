d8fbf8c2f9aa77f586c4b9452fc88ec154d3376d
Roel De Coninck <roeldeconinck@gmail.com>
2016-05-18 08:59:02 +0200

    Comments and bugfixes in docker-run shell scripts

--- docker-run.sh
+++ docker-run.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
+# On Mac, start docker daemon if not running
 if docker-machine  > /dev/null 2>&1; then
     eval $(docker-machine env default)
 fi
+# Stop and/or remove existing opengrid-dev container, if any
 if docker ps -a | grep -q opengrid-release; then
 	echo "Stopping and/or removing existing opengrid-release container."
 	docker stop opengrid-release > /dev/null
 	docker rm opengrid-release > /dev/null
 fi
+# Start the docker, publish port 8888 to host and mount current folder to /usr/local/opengrid in the container
 CID=$(docker run -d -p 8888:8888 -v $(pwd -P):/usr/local/opengrid --name opengrid-release opengrid/release:latest)
+
 echo "Opengrid notebook server running on http://$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' ${CID}):8888"
 echo "Please enter this in your browser to access the jupyter notebooks."
 
