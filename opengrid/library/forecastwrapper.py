0813c747cc1d1b79b14f6a9ee483834844a41d81
Jan Pecinovsky <janpecinovsky@gmail.com>
2017-02-02 22:18:13 +0100

    irradiance on a tilted surface for the hourly dataframe

--- opengrid/library/forecastwrapper.py
+++ opengrid/library/forecastwrapper.py
@@ -12,7 +12,6 @@ from tqdm import tqdm
 import os
 import pickle
 
-
 from .misc import dayset, calculate_temperature_equivalent, \
     calculate_degree_days
 from opengrid import config
@@ -234,17 +233,34 @@ class Weather():
 
         return frame
 
-    def hours(self):
+    def hours(self, irradiances=None):
         """
         Create a dataframe with all weather data in hourly resolution
 
+        Parameters
+        ----------
+        irradiances : list[tuple], optional
+            Calculate the global solar irradiance for multiple surfaces with a
+            given orientation and tilt
+            List with tuples. tuple = (orientation, tilt)
+            Orientation in degrees from north (0 = north, 90 = east, ...)
+            Tilt in degrees (0 = horizontal, 90 = vertical)
+
         Returns
         -------
-        Pandas Dataframe
+        pandas.DataFrame
         """
         day_list = [self._forecast_to_hour_series(forecast) for forecast in self.forecasts]
         frame = pd.concat(day_list)
-        return self._fix_index(frame).sort_index().truncate(before=self.start, after=self.end)
+        frame = self._fix_index(frame)
+        frame.sort_index(inplace=True)
+        frame = frame.truncate(before=self.start, after=self.end)
+
+        if irradiances is not None:
+            for ir in irradiances:
+                frame = self._add_irradiance(frame=frame, orient=ir[0], tilt=ir[1])
+
+        return frame
 
     def _get_forecast(self, date):
         """
@@ -362,6 +378,11 @@ class Weather():
                          }
             solar = {new_names[key]: val for key, val in solar.items()}
             j.update(solar)
+
+            # workaround for the 90 bug by Dark Sky
+            # add 90 and take modulo 360 to stay between 0 and 360
+            j.update({'SolarAzimuth': (j.get('SolarAzimuth') + 90) % 360})
+
             return j
 
     def _fix_index(self, frame):
@@ -556,3 +577,71 @@ class Weather():
             return pickle.load(open(path, "rb"))
         else:
             return None
+
+    @staticmethod
+    def irradiance_on_tilted_surface(dni, dhi, altitude, azimuth, orient, tilt):
+        """
+        Calculate the global solar radiation on a tilted surface
+
+        Parameters
+        ----------
+        dni : pandas.Series
+            Direct Normal Irradiance
+        dhi : pandas.Series
+            Diffuse Horizontal Irradiance
+        altitude : pandas.Series
+            altitude of the sun above the horizon (in degrees)
+            0 = horizon, 90 = right above
+        azimuth : pandas.Series
+            location of the sun in respect to North (in degrees)
+            0 = North, 90 = East, 180 = South, 270 = West
+        orient : int|float
+            orientation of the surface
+            0 = North, 90 = East, 180 = South, 270 = West
+        tilt : int|float
+            tilt of the surface
+            0 = Horizontal, 90 = Vertical
+
+        Returns
+        -------
+        pandas.Series
+        """
+        a = np.radians(altitude.astype(float))
+        b = np.radians(float(tilt))
+        c = np.radians(float(orient))
+        d = np.radians(azimuth.astype(float))
+
+        direct = dni * (np.cos(a) * np.sin(b) * np.cos(c - d) + np.sin(a) * np.cos(b))
+        direct[direct < 0] = 0
+        return direct + dhi
+
+    def _add_irradiance(self, frame, orient, tilt):
+        """
+        Add a column to the frame with the global irradiance on a surface for
+        a given orientation and tilt
+
+        Parameters
+        ----------
+        frame : pandas.DataFrame
+        orient : float|int
+            in degrees from north
+        tilt : float|int
+            in degrees from horizontal
+
+        Returns
+        -------
+        pandas.DataFrame
+        """
+        new_col = self.irradiance_on_tilted_surface(
+            dni=frame.DirectNormalIrradiance,
+            dhi=frame.DiffuseHorizontalIrradiance,
+            altitude=frame.SolarAltitude,
+            azimuth=frame.SolarAzimuth,
+            orient=orient,
+            tilt=tilt
+        )
+        name = "GlobalIrradianceO{}T{}".format(int(orient), int(tilt))
+
+        frame[name] = new_col
+
+        return frame
