82b29540a67575523dceb64de7b3c50e080e9ac0
saroele <roeldeconinck@gmail.com>
2017-03-23 20:54:49 +0100

    Job to cache anonymous houseprint now saves to the data folder as specified by the configuration.

--- opengrid/recipes/cache_anonymous_houseprint.py
+++ opengrid/recipes/cache_anonymous_houseprint.py
@@ -4,13 +4,18 @@ Script to cache anonymous houseprint data into hp_anonymous.pkl
 
 Created on 05/07/2014 by Roel De Coninck
 """
+import os
 
 from opengrid.library.houseprint import houseprint
+from opengrid import config
+c = config.Config()
+
 
 ##############################################################################
 
 hp = houseprint.Houseprint()
 print('Sensor data fetched')
 
-hp.save('hp_anonymous.pkl')
-print('Houseprint saved to hp_anonymous.pkl')
+filename = os.path.join(c.get('data', 'folder'), 'hp_anonymous.pkl')
+hp.save(filename)
+print('Houseprint saved to {}'.filename)
