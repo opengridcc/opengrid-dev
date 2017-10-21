6fc031ec0e6055312afcd1ee0c31becccbc15689
saroele <roeldeconinck@gmail.com>
2017-10-21 10:57:03 +0200

    - Bugfix in setup.py - New location of config in Dockerfile.release

--- setup.py
+++ setup.py
@@ -63,8 +63,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=['opengrid'],
-    #packages=find_packages(),
+    packages=find_packages(),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
